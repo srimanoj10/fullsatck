067881378bed5427c4463a3d1a0193406e989819
srimanoj10 <srimanojr.22cse@kongu.edu>
2024-12-28 11:50:06 +0530

    power1

---
+++ operator.js
@@ -0,0 +1,42 @@
+var a = 10
+var b = 10
+
+if(a==b)
+{
+    console.log("both values are same")
+}
+else{
+    console.log("both values are not same")
+}
+
+//arithmetic operators
+add=a+b
+console.log(add)
+sub=a-b
+console.log(sub)
+mul=a*b
+console.log(mul)
+div=a/b
+console.log(div)
+mod=a%b
+console.log(mod)
+
+// Assignment Operators
+b += 10; 
+console.log("After += Operator:", b); 
+
+// Comparison Operators
+console.log("a > b:", a > b); 
+console.log("a == b:", a == b); 
+console.log("a === b:", a === b); 
+
+// Logical Operators
+let x = true, y = false;
+console.log("x && y:", x && y); 
+console.log("x || y:", x || y); 
+console.log("!x:", !x);  
+
+// Unary Operators
+console.log("Increment:", ++b); 
+console.log("Decrement:", --b); 
+
